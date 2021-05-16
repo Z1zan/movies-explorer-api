6ac443b4a08ef0420bb0fb3bf09f4a7187b5ec06
Ivan Nemirich <bebded@gmail.com>
2021-05-16 21:27:44 +0700

    add: изменение информации пользователя

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUserInfo,
+  updateUserInfo,
   signOut,
 } = require('../controllers/users');
 
 router.get('/me', getUserInfo);
+router.post(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      name: Joi.string().min(2).max(30).required(),
+    }),
+  }),
+  updateUserInfo,
+);
 router.delete('/signout', signOut);
 
 module.exports = router;
