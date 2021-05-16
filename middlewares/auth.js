c9c9c686710258e09f2bdb340b72f8a1da3e8d78
Ivan Nemirich <bebded@gmail.com>
2021-05-16 17:42:46 +0700

    chore: для проверки сервера на другом пк

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,10 @@ const AuthError = require('../errors/authError');
 const secret = process.env;
 
 function auth(req, res, next) {
+  console.log(req.cookies);
   try {
     req.user = jwt.verify(
       req.cookies.jwt,
-      console.log("jwt", req.cookies),
       secret.NODE_ENV === 'production'
         ? secret.JWT_SECRET
         : 'dev-secret',
